accca372ca3b418fced0b6e6d55df9fe56ccb7ae
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-11 12:00:12 +0000

    adding new file

--- Terraform-Immutable/main.tf
+++ Terraform-Immutable/main.tf
@@ -5,7 +5,7 @@ module "frontend" {
     component   = "frontend"
     port        = 8080
     is_internal = "true"
-    priority    = 200
+    priority    = 300
     APP_VERSION = var.APP_VERSION
     max_size    = var.max_size
     min_size    = var.min_size
