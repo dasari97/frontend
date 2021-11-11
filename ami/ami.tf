ace956c4a4cbd08adc61eb72655007de5e3e3937
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-11 01:19:46 +0000

    adding new file

--- ami/ami.tf
+++ ami/ami.tf
@@ -1,13 +1,13 @@
 provider "aws" {
     region = "us-east-1"
 }
-
+terraform {
 backend "s3" {
     bucket = "dasari1998"
     key    = "ami/frontend/dev/dev_state_file_backup"
     region = "us-east-1"
+    }
 }
-
 module "ami" {
     source = "git::https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-ami"
     component   = "frontend"
